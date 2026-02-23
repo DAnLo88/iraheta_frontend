91aeb70ae1d1a01f0c3091b2f294313aad94e128
DAnLo88 <danilo8812@gmail.com>
2026-02-22 20:25:23 -0600

    Slider Home

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,8 +1,13 @@
-import { Component, signal,OnInit } from '@angular/core';
+import { Component, signal,OnInit, AfterViewInit, ElementRef } from '@angular/core';
 import { WordpressService } from '../../services/wordpress.service';
 // import { LottieComponent } from 'ngx-lottie';
 import { DomSanitizer, SafeHtml } from '@angular/platform-browser';
+// import function to register Swiper custom elements
+import { register } from 'swiper/element/bundle';
+// register Swiper custom elements
+register();
 
+import { Router } from '@angular/router';
 
 @Component({
   standalone: true,
@@ -14,22 +19,52 @@ export class HomeComponent implements OnInit {
 safeContent!: SafeHtml;
   homePage: any;
  protected readonly title = signal('iraheta-site');
-  constructor(private wpService: WordpressService, private sanitizer: DomSanitizer) {}
+  constructor(
+      private el: ElementRef,
+    private wpService: WordpressService, 
+    private sanitizer: DomSanitizer,
+    private router: Router
+  ) {}
 
   ngOnInit(): void {
-   this.wpService.getPageBySlugHome().subscribe(res => {
+    this.wpService.getPageBySlugHome().subscribe(res => {
     this.homePage = res[0];
 
     this.safeContent = this.sanitizer.bypassSecurityTrustHtml(
       this.homePage.content.rendered
     );
-  });
+   });
+  }
 
 
+  ngAfterViewInit() {
+    this.el.nativeElement.addEventListener('click', (event: any) => {
+      const link = event.target.closest('a.spa-link');
+
+      if (!link) return;
+
+      event.preventDefault();
+
+      const route = link.getAttribute('href');
+      this.router.navigateByUrl(route);
+    });
   }
   //     lottieOptions = {
   //   path: 'assets/animations/website_under.json', // ruta de tu archivo JSON
   //   loop: true,
   //   autoplay: true
   // };
+
+
+
+  handleSpaClick(event: Event) {
+    event.preventDefault();
+
+    const target = event.currentTarget as HTMLAnchorElement;
+    const route = target.getAttribute('href');
+
+    if (route) {
+      this.router.navigateByUrl(route);
+    }
+  }
 }
